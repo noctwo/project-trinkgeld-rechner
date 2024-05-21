1909d8d9430b713ebecbfa8efeb65a520c7002e4
noctwo <mr.evok@gmail.com>
2024-05-21 16:20:27 +0200

    project and readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project Name
+> Live demo [_here_](https://www.example.com).
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Screenshots](#screenshots)
+
+
+## General Information
+Project TypeScript Trinkgeld-Rechner
+https://supercode.notion.site/Project-TS-Trinkgeldrechner-d37bd545188c44e2b9682bbfbda713b7
+
+
+
+## Technologies Used
+- HTML5
+- CSS3
+- TypeScript
+- Vite
+
+
+## Features
+- Höhe des Trinkgeldes nach Auswahl der Servicequalität
+- Berechnung der Höhe d. Trinkgeldes
+- Berechnung des Gesamtpreises
+- Berechnung des Preises pro Person
+
+
+## Screenshots
+![Example screenshot](.src/img/flowchart.png)
+
+
+
---
+++ src/calculator.ts
@@ -0,0 +1,38 @@
+let amountInput = document.getElementById("amount-input") as HTMLInputElement;
+let personsInput = document.getElementById("persons-input") as HTMLInputElement;
+let selectedService = document.getElementById("selected-service") as HTMLInputElement;
+let calculateBtn = document.getElementById("calculate-btn") as HTMLElement;
+let outputArea = document.querySelector("#output-area") as HTMLElement;
+
+
+calculateBtn?.addEventListener("click", ()=>{
+    calculateTip();
+});
+
+
+let serviceRating = 0;
+function getServiceRating (){
+    
+    if (selectedService.value === "Gut"){
+        serviceRating = 20;
+    } else if (selectedService.value === "So mittel"){
+        serviceRating = 10;
+    } else if (selectedService.value === "Mies"){
+        serviceRating = 2;
+    }
+}    
+
+function calculateTip (){
+    getServiceRating ();
+    let tipAmount = (Number(amountInput.value) / 100) * serviceRating;
+    let totalBillAmount = (Number(amountInput.value)) + tipAmount;
+    let amountPerPerson = totalBillAmount / Number(personsInput.value) ;
+    let billAmountOutput = outputArea?.appendChild(document.createElement("p"));
+    billAmountOutput.innerHTML = `Höhe der Rechnung: ${totalBillAmount}`;
+
+    let tipAmountOutput = outputArea?.appendChild(document.createElement("p"));
+    tipAmountOutput.innerHTML = `Höhe des Trinkgeldes: ${tipAmount}`;
+
+    let billAmountPerPerson = outputArea?.appendChild(document.createElement("p"));
+    billAmountPerPerson.innerHTML = `Betrag pro Person: ${amountPerPerson}`;
+};
